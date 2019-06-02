d5d4548d809193c60e20491e97638aeab38b38b8
asingjr2 <asingjr2@gmail.com>
2019-06-02 19:47:48 -0400

    updated rest_views to respond with serializer data for get reuests. Need to get tasks going next

--- content/rest_views.py
+++ content/rest_views.py
@@ -1,29 +1,40 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.contrib.auth.models import User
+
+
+from .models import (
+    Movie, 
+    Food,
+    Wine
+)
+
+from .seriliazers import (
+    MovieSerializer,
+    FoodSerializer,
+    WineSerializer
+)
 
 
 class MovieAPIView(APIView):
-    def get(self, request):
-        data = {
-            'message': 'Movie page'
-        }
+    def get(self, request, format=None):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
 
-        return Response(data)
+        return Response(serializer.data)
 
 
 class FoodAPIView(APIView):
-    def get(self, request):
-        data = {
-            'message': 'Food page'
-        }
-        
-        return Response(data)
+    def get(self, request, format=None):
+        foods = Food.objects.all()
+        serializer = FoodSerializer(foods, many=True)
+
+        return Response(serializer.data)
 
 
 class WineAPIView(APIView):
-    def get(self, request):
-        data = {
-            'message': 'Wine page'
-        }
-        
-        return Response(data)
+    def get(self, request, format=None):
+        wines = Wine.objects.all()
+        serializer = WineSerializer(wines, many=True)
+
+        return Response(serializer.data)
